6f571d968e30b8467e568617d4347070858e27a8
pitelinalexey <pitelinalexey@mail.ru>
2020-10-09 21:43:10 +0300

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
-
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
-
         final int MAX_CONTAINERS_IN_TRUCK = 12;
         final int MAX_BOXES_IN_CONTAINER = 27;
 
